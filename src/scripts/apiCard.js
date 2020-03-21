bf5dfb60f320af6a56ea454163f9bf99f6316f9a
Atichka <atichka@MacBook-Air-Ekaterina.local>
2020-03-22 02:03:56 +0300

    add code js

---
+++ src/scripts/apiCard.js
@@ -0,0 +1,19 @@
+class ApiCard {
+  constructor(baseUrl, key) {
+    this.baseUrl = baseUrl;
+    this.key = key;
+  }
+
+  getCard(from, to, query) {
+    this.url = `${this.baseUrl}q=${query}&from=${from}&to=${to}&pageSize=100&apiKey=${this.key}&language=ru`;
+    return fetch(this.url)
+    .then(res => {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject(`Что-то пошло не так: ${res.status}`);
+    })
+  }
+}
+
+export {ApiCard};
